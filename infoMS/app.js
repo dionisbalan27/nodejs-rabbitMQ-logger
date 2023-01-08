3af93fad066941b1cc44d2de91c977f61deeae1b
dionisbp <dionisbp27@gmail.com>
2023-01-08 23:40:46 +0700

    add new folder for case consume messages from the queue

---
+++ infoMS/app.js
@@ -0,0 +1,20 @@
+const amqp = require("amqplib");
+
+async function consumeMessages() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  await channel.assertExchange("logExchange", "direct");
+
+  const q = await channel.assertQueue("InfoQueue");
+
+  await channel.bindQueue(q.queue,"logExchange","Info");
+
+  channel.consume(q.queue, (msg) => {
+    const data = JSON.parse(msg.content);
+    console.log(data);
+    channel.ack(msg);
+  });
+}
+
+consumeMessages();
